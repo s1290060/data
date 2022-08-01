fda2cc92b391c51c404cb5117e05293720dadf84
k <s1290060u-aizu.ac.jp>
2022-08-02 04:27:03 +0900

    Final revision

---
+++ data_ex4_3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int getrandom(int,int);
+int main(){
+    char name[100];
+    printf("Who are you?\n");
+    scanf("%s",name);
+    printf("Hello,%s!\n",name);
+    int num;
+    int head = 0, tail = 0;
+    for(int i = 1; i < 4; i++){
+        num = getrandom(0,2);
+        if(num == 0){
+            printf("Round %d:Heads\n",i);
+            head++;
+        }else{
+            printf("Round %d:Tails\n",i);
+            tail++;
+        }
+    }
+    printf("Heads:%d Tails:%d\n",head,tail);
+    if(head > tail){
+        printf("%s won\n",name);
+    }else{
+        printf("%s lost\n",name);
+    }
+    return 0;
+}
+
+int getrandom(int min, int max){
+    return min + rand() % max;
+}
